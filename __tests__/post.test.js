bfd9fdbbe277fb485484d3a5719b6aed0ab80873
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-06-30 19:59:36 -0400

    test passing for post POST route

--- __tests__/post.test.js
+++ __tests__/post.test.js
@@ -2,29 +2,49 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import UserService from '../lib/services/UserService.js';
+// import Post from '../lib/models/Post.js';
 
-const agent = request.agent(app);
-
-const kitten = { 
-  photoUrl: 'http://placekitten.com/200/300',
-  caption: 'its a kitten',
-  tags: '#kitten' };
 
 describe('demo routes', () => {
-  beforeAll(() => {
-    return setup(pool);
+
+  let user = {};
+  let agent;
+
+  beforeAll(async() => {
+    await setup(pool);
+    agent = request.agent(app);
+    user = await UserService.create({
+      username: 'nelly',
+      password: 'password',
+      profilePhotoUrl: 'http://placekitten.com/200/300'
+    });
+    await agent.post('/api/v1/auth/login')
+      .send({
+        username: 'nelly',
+        password: 'password'
+      });
   });
 
-  it('creates a post vis POST', async () => {
-    const res = await agent 
-      .post('/api/v1/post')
-      .send(kitten);
+  it('creates a post via POST', async () => {
+    const res = await agent
+      .post('/api/v1/posts')
+      .send({
+        userId: '1',
+        photoUrl: 'http://placekitten.com/200/300',
+        caption: 'its a kitten',
+        tags: ['kitten', 'lol']
+      });
 
     expect(res.body).toEqual({
-      id: 1,
+      id: '1',
+      userId: '1',
       photoUrl: 'http://placekitten.com/200/300',
       caption: 'its a kitten',
-      tags: '#kitten'
+      tags: ['kitten', 'lol']
     });
   });
+
+
+
 });
