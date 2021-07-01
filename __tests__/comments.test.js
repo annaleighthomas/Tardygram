e6da51c8d2e4d941f3de0981dbdc8cc787af8b35
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-07-01 15:06:02 -0400

    passing delete routes and test

--- __tests__/comments.test.js
+++ __tests__/comments.test.js
@@ -5,7 +5,6 @@ import app from '../lib/app.js';
 import UserService from '../lib/services/UserService.js';
 import Post from '../lib/models/Post.js';
 import Comment from '../lib/models/Comment.js';
-import User from '../lib/models/User.js';
 
 
 describe('comment routes', () => {
@@ -42,18 +41,37 @@ describe('comment routes', () => {
       .post('/api/v1/comments')
       .send({
         commentBy: user.id,
-        post: post.id,
+        postId: post.id,
         comment: 'what a cuuuuute kitty'
       });
 
     expect(res.body).toEqual({
       id: '1',
       commentBy: user.id,
-      post: post.id,
+      postId: post.id,
       comment: 'what a cuuuuute kitty'
     });
   });
 
+  it('deletes a comment using DELETE', async () => {
+    post = await Post.insert({
+      userId: user.id,
+      photoUrl: 'http://placekitten.com/200/300',
+      caption: 'its a cute',
+      tags: ['cool kat', 'nice'] 
+    });
 
-  
+    const comment = await Comment.insert({
+      id: '1',
+      commentBy: user.id, 
+      post_id: post.id,
+      comment: 'cute kitty'
+    });
+
+    const res = await agent 
+      .delete(`/api/v1/comments/${comment.id}`)
+      .send(comment);
+
+    expect(res.body).toEqual(comment);
+  });
 });
